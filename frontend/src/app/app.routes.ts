8d767e8abcb410ef722a8a170cfdc35192a19ff6
Anwar <anwarhasnaoui967@gmail.com>
2026-04-14 15:38:10 +0000

    Fixing some errors

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Login} from './components/login/login';
+import { Register} from './components/register/register';
+import { Slots } from './components/slots/slots';
+import { Dashboard} from './components/dashboard/dashboard';
